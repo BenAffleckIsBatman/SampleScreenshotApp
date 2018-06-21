53bca414432eb2e1b269b7e5ed8604080d6d0bfa
Sebastian Zolg <Sebastian.Zolg@swisscom.com>
2018-06-21 21:26:04 +0200

    Initial commit

---
+++ src/ScreenCaptureApp1/MainPage.xaml.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.Graphics;
+using Windows.Graphics.Capture;
+using Windows.Graphics.DirectX;
+using Windows.Storage;
+using Windows.System;
+using Windows.UI;
+using Windows.UI.Composition;
+using Microsoft.Graphics.Canvas;
+
+using Windows.UI.Core;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace ScreenCaptureApp1
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            await StartCaptureAsync();
+            //    var screenshot = new Screenshot();
+            //    await screenshot.PickItemAsync();
+            //    await screenshot.TakeAsync();
+            //System.Threading.Thread.Sleep(2000);
+            //    await screenshot.SaveToFile(ApplicationData.Current.TemporaryFolder, "file");
+
+
+
+        }
+        // Capture API objects.
+        private SizeInt32 _lastSize;
+        private GraphicsCaptureItem _item;
+        private Direct3D11CaptureFramePool _framePool;
+        private GraphicsCaptureSession _session;
+        private CanvasBitmap _canvasBitmap;
+
+        // Non-API related members.
+        private CanvasDevice _canvasDevice;
+        private CompositionDrawingSurface _surface;
+
+        public async Task StartCaptureAsync()
+        {
+            _canvasDevice = CanvasDevice.GetSharedDevice();
+            // The GraphicsCapturePicker follows the same pattern the 
+            // file pickers do. 
+            var picker = new GraphicsCapturePicker();
+            GraphicsCaptureItem item = await picker.PickSingleItemAsync();
+
+            // The item may be null if the user dismissed the 
+            // control without making a selection or hit Cancel. 
+            if (item != null)
+            {
+                StartCaptureInternal(item);
+            }
+        }
+
+        private void  StartCaptureInternal(GraphicsCaptureItem item)
+        {
+            // Stop the previous capture if we had one.
+            StopCapture();
+
+            _item = item;
+            _lastSize = _item.Size;
+
+            _framePool = Direct3D11CaptureFramePool.Create(
+               _canvasDevice, // D3D device 
+               DirectXPixelFormat.B8G8R8A8UIntNormalized, // Pixel format 
+               1, // Number of frames 
+               _item.Size); // Size of the buffers 
+
+            _framePool.FrameArrived += (s, a) =>
+            {
+                // The FrameArrived event is raised for every frame on the thread
+                // that created the Direct3D11CaptureFramePool. This means we 
+                // don't have to do a null-check here, as we know we're the only 
+                // one dequeueing frames in our application.  
+
+                // NOTE: Disposing the frame retires it and returns  
+                // the buffer to the pool.
+
+                    using (var frame = _framePool.TryGetNextFrame())
+                    {
+                        ProcessFrame(frame);
+                    }
+  
+            };
+
+            _item.Closed += async (s, a) =>
+            {
+                 StopCapture();
+               // await SaveScreenshot();
+            };
+
+            _session = _framePool.CreateCaptureSession(_item);
+            _session.StartCapture();
+        }
+
+        public void StopCapture()
+        {
+            _session?.Dispose();
+            _framePool?.Dispose();
+            _item = null;
+            _session = null;
+            _framePool = null;
+        }
+
+        private async void ProcessFrame(Direct3D11CaptureFrame frame)
+        {
+            // Resize and device-lost leverage the same function on the
+            // Direct3D11CaptureFramePool. Refactoring it this way avoids 
+            // throwing in the catch block below (device creation could always 
+            // fail) along with ensuring that resize completes successfully and 
+            // isn’t vulnerable to device-lost.   
+            bool needsReset = false;
+            bool recreateDevice = false;
+
+            if ((frame.ContentSize.Width != _lastSize.Width) ||
+                (frame.ContentSize.Height != _lastSize.Height))
+            {
+                needsReset = true;
+                _lastSize = frame.ContentSize;
+            }
+
+            try
+            {
+                // Take the D3D11 surface and draw it into a  
+                // Composition surface.
+
+                // Convert our D3D11 surface into a Win2D object.
+                _canvasBitmap = CanvasBitmap.CreateFromDirect3D11Surface(
+                    _canvasDevice,
+                    frame.Surface);
+
+
+
+                //FillSurfaceWithBitmap(_surface, canvasBitmap);
+            }
+            // This is the device-lost convention for Win2D.
+            catch (Exception e) when (_canvasDevice.IsDeviceLost(e.HResult))
+            {
+                // We lost our graphics device. Recreate it and reset 
+                // our Direct3D11CaptureFramePool.  
+                needsReset = true;
+                recreateDevice = true;
+            }
+
+            if (needsReset)
+            {
+                ResetFramePool(frame.ContentSize, recreateDevice);
+            }
+        }
+
+        private async Task SaveScreenshot()
+        {
+            // Helper that handles the drawing for us, not shown. 
+            var targetFile = await ApplicationData.Current.TemporaryFolder.CreateFileAsync($"{_item.DisplayName}-{new Random().Next()}.png", CreationCollisionOption.ReplaceExisting);
+            await _canvasBitmap.SaveAsync(targetFile.Path);
+        }
+
+        private void ResetFramePool(SizeInt32 size, bool recreateDevice)
+        {
+            do
+            {
+                try
+                {
+                    if (recreateDevice)
+                    {
+                        _canvasDevice = new CanvasDevice();
+                    }
+
+                    _framePool.Recreate(
+                        _canvasDevice,
+                        DirectXPixelFormat.B8G8R8A8UIntNormalized,
+                        2,
+                        size);
+                }
+                // This is the device-lost convention for Win2D.
+                catch (Exception e) when (_canvasDevice.IsDeviceLost(e.HResult))
+                {
+                    _canvasDevice = null;
+                    recreateDevice = true;
+                }
+            } while (_canvasDevice == null);
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            StopCapture();
+            SaveScreenshot();
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            SaveScreenshot();
+        }
+    }
+}
---
+++ src/ScreenCaptureApp1/Screenshot.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Devices.AllJoyn;
+using Windows.Graphics;
+using Windows.Graphics.Capture;
+using Windows.Graphics.DirectX;
+using Windows.Storage;
+using Windows.UI;
+using Microsoft.Graphics.Canvas;
+
+namespace ScreenCaptureApp1
+{
+    public class Screenshot
+    {
+
+
+
+
+
+
+
+        //private CanvasBitmap _screenshotBitmap;
+        //private GraphicsCaptureItem _item;
+        //private Direct3D11CaptureFramePool _framePool;
+        //private readonly CanvasDevice _canvasDevice;
+        //private GraphicsCaptureSession _session;
+        //private TaskCompletionSource<bool> _screenshotTaken;
+        
+
+        //public Screenshot()
+        //{
+        //    _canvasDevice = CanvasDevice.GetSharedDevice();
+        //}
+
+        //public async Task TakeAsync()
+        //{
+        //    if (_item == null)
+        //         return;
+
+        //    _screenshotTaken = new TaskCompletionSource<bool>();
+          
+        //    _framePool.FrameArrived += (sender, args) =>
+        //    {
+        //        using (var frame = _framePool.TryGetNextFrame())
+        //        {
+        //            _screenshotBitmap = CanvasBitmap.CreateFromDirect3D11Surface(_canvasDevice, frame.Surface);
+        //        }
+        //        _framePool.Dispose();
+        //    };
+
+        //    CreateAndStartCaptureSession(_framePool);
+        //    await _screenshotTaken.Task;
+        //    StopCapture();
+        //}
+
+        //public async Task PickItemAsync()
+        //{
+        //    var picker = new GraphicsCapturePicker();
+        //    _item = await picker.PickSingleItemAsync();
+
+        //    if (_item != null)
+        //        _framePool = CreateFramePool(_canvasDevice);
+
+        //}
+
+        //public async Task<StorageFile> SaveToFile(StorageFolder folder, string fileName)
+        //{
+        //    CanvasRenderTarget offscreen = new CanvasRenderTarget(CanvasDevice.GetSharedDevice(), _screenshotBitmap.SizeInPixels.Width, _screenshotBitmap.SizeInPixels.Height, 96);
+
+        //    using (CanvasDrawingSession ds = offscreen.CreateDrawingSession())
+        //    {
+        //        ds.Clear(Colors.White);
+        //        ds.DrawImage(_screenshotBitmap);
+        //    }
+            
+        //    var newFile = await folder.CreateFileAsync($"{fileName}.png", CreationCollisionOption.ReplaceExisting);
+        //    if (newFile != null)
+        //        await offscreen.SaveAsync(newFile.Path);
+
+        //    return newFile;
+        //}
+
+        //private Direct3D11CaptureFramePool CreateFramePool(CanvasDevice canvasDevice)
+        //{
+        //    return  Direct3D11CaptureFramePool.Create(
+        //        canvasDevice, // D3D device 
+        //        DirectXPixelFormat.B8G8R8A8UIntNormalized, // Pixel format 
+        //        1, // Number of frames 
+        //        _item.Size); // Size of the buffers 
+        //}
+
+        //private void CreateAndStartCaptureSession(Direct3D11CaptureFramePool framePool)
+        //{
+        //    _session = framePool.CreateCaptureSession(_item);
+        //    _session.StartCapture();
+        //}
+
+        //public void StopCapture()
+        //{
+        //    _session?.Dispose();
+        //    //_framePool?.Dispose();
+        //    _item = null;
+        //    _session = null;
+        //    //_framePool = null;
+        //    _screenshotTaken = null;
+        //}
+
+
+    }
+}
